becedb832d5c84dcfbba361d03b0ebbdbec3e722
LauM727 <LauraMarcelaM.mp26@gmail.com>
2024-02-01 19:13:57 -0500

    Exercise 1 - 2 commit

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export function Body(){
+    return(
+        <body>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo natus, nam ex assumenda adipisci quam. Voluptatibus ad quae quibusdam ex ipsa quisquam odio magni eligendi sunt tenetur. Molestiae, veniam modi.</p>
+        </body>
+    )
+}
+
+
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export function Header(){
+    return(
+        <header>
+            <h1>Hello World</h1>
+        </header>
+    )
+}
